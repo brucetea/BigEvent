acf69af78ec6aeb533c5fc315f1814f4cf8a613c
Zhichuang.Chang <18004462689@163.com>
2026-05-26 00:58:22 +0800

    feat: first commit

---
+++ src/views/article/components/ChannelSelet.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { artGetChannelsService } from '@/api/article'
+import { ref } from 'vue'
+
+const channelList = ref([])
+const selected = ref('')
+
+
+
+const getList = async () => {
+  const res = await artGetChannelsService()
+  channelList.value = res.data.data
+}
+getList()
+</script>
+
+<template>
+  {{ obj?.name  }}
+  <el-select v-model="selected" placeholder="请选择">
+    
+    <el-option
+      v-for="item in channelList"
+      :key="item.id"
+      :label="item.cate_name"
+      :value="item.cate_alias"
+    />
+  </el-select>
+</template>
